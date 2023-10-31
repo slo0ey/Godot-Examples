7d7bb6871e1ab536dc41c7c4b1380438023dc69e
slo0ey <rurchi1206@gmail.com>
2023-10-31 23:41:55 +0900

    Gun Shot 프로젝트 업데이트 중..

--- 3D/Gun Shot/player.gd
+++ 3D/Gun Shot/player.gd
@@ -1,37 +1,41 @@
 extends CharacterBody3D
 
+@onready var MainCamera = $MainCamera
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
-@onready var camera := $Camera3D 
-@onready var gun := $Camera3D/Pistol
+var CameraRotation = Vector2.ZERO
+var MouseSensitivity = 0.005
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
-
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
-
-func _unhandled_input(event):
-	if Input.is_action_just_pressed("quit"):
-		get_tree().quit()
+func _input(event):
+	if event .is_action_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	
 	if event is InputEventMouseMotion:
-		rotate_y(-event.relative.x * .005)
-		camera.rotate_x(-event.relative.y * .005)
-		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-89), deg_to_rad(89))
+		var MouseEvent = event.relative * MouseSensitivity
+		CameraLook(MouseEvent)
 
+func CameraLook(Movement: Vector2):
+	CameraRotation += Movement
+	CameraRotation.y = clamp(CameraRotation.y, -1.5, 1.2)
+	
+	transform.basis = Basis()
+	MainCamera.transform.basis = Basis()
+	
+	rotate_object_local(Vector3(0, 1, 0), -CameraRotation.x)
+	MainCamera.rotate_object_local(Vector3(1, 0, 0), -CameraRotation.y)
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
-		
-	if Input.is_action_just_pressed("shoot"):
-		gun.shoot()
 
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
@@ -39,7 +43,7 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("left", "right", "forward", "back")
+	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
